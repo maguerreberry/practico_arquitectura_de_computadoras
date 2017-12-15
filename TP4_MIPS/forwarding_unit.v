0ca4edba327732f655497a90843e08f5e9aa9248
Facundo <facundojmaero@gmail.com>
2017-12-15 20:24:04 -0300

    maldita letra

--- TP4_MIPS/forwarding_unit.v
+++ TP4_MIPS/forwarding_unit.v
@@ -35,16 +35,6 @@ module forwarding_unit #(
 	output reg [1:0] control_muxB 	// control mux entrada B de la ALU
     );
 
-	// manejo primer mux
-	// assign control_muxA = (register_write_4_5 == 1 & rd_4_5 == rs_2_3 & (register_write_3_4 == 0 | rd_3_4 != rs_2_3)) ? 2'b10
-	// 					: (register_write_3_4 == 1 & rd_3_4 == rs_2_3) ? 2'b01
-	// 						/* default */	: 2'b00;
-
-	// manejo segundo mux
-	// assign control_muxB = (register_write_4_5 == 1 & rd_4_5 == rt_2_3 & (register_write_3_4 == 0 | rd_3_4 != rt_2_3)) ? 2'b10
-	// 					: (register_write_3_4 == 1 & rd_3_4 == rt_2_3) ? 2'b01
-	// 						/* default */	: 2'b00;
-
 	always @(*) begin
 		if(register_write_4_5 == 1 & rd_4_5 == rs_2_3 & (register_write_3_4 == 0 | rd_3_4 != rs_2_3))
 			control_muxA <= 2'b10;
@@ -56,7 +46,7 @@ module forwarding_unit #(
 
 		if(register_write_4_5 == 1 & rd_4_5 == rt_2_3 & (register_write_3_4 == 0 | rd_3_4 != rt_2_3))
 			control_muxB <= 2'b10;
-		else if (register_write_3_4 == 1 & rd_3_4 == rs_2_3)
+		else if (register_write_3_4 == 1 & rd_3_4 == rt_2_3)
 			control_muxB <= 2'b01;
 		else
 			control_muxB <= 2'b00;
