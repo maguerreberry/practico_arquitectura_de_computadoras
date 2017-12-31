fa1ec3efb9ce9dbe8bab00819dc0fb02f987f3a4
Facundo <facundojmaero@gmail.com>
2017-12-31 17:35:04 -0300

    python

--- TP4_MIPS/serial.py
+++ TP4_MIPS/serial.py
@@ -8,7 +8,15 @@ h = True
 nro_registros = 32
 posiciones_memoria = 16
 
-file = "practico_arquitectura_de_computadoras/TP4_MIPS/program.hex"
+# codigos a enviar por UART
+StartSignal        = 0b00000001
+ContinuosSignal    = 0b00000010
+StepByStepSignal   = 0b00000011
+ReProgramSignal    = 0b00000101
+StepSignal         = 0b00000110
+
+# file = "practico_arquitectura_de_computadoras/TP4_MIPS/program.hex"
+file = "program.hex"
 
 def concat_bin(byte_4, byte_3, byte_2, byte_1):
 	
@@ -36,6 +44,11 @@ def print_registro_32(mensaje, registro):
 
 	print # para el newline
 
+def sendInstruction(instruction):
+	for x in xrange(0,8,2):
+		print instruction[x:x+2]
+		# sent = ser.write(instruction[x:x+2])
+
 print """
  ____                                    _              __  __ ___ ____  ____ 
 |  _ \ _ __ ___   ___ ___  ___  __ _  __| | ___  _ __  |  \/  |_ _|  _ \/ ___| 
@@ -48,24 +61,23 @@ print "\nIngenieria en Computacion 2017\nAutores:\n	Matthew Aguerreberry\n	Facun
 
 raw_input("Presione una tecla para iniciar la carga del programa ensamblador por UART")
 
-com = 0
+# com = 0
 
-for i in xrange(1,10):
-	try:
-		puerto = 'COM' + str(i)
-		ser = serial.Serial(puerto)
-		com = i
-		break
-	except Exception as e:
-		if i == 9:
-			print "No puedo encontrar la FPGA"
-			quit()
+# for i in xrange(1,10):
+# 	try:
+# 		puerto = 'COM' + str(i)
+# 		ser = serial.Serial(puerto)
+# 		com = i
+# 		break
+# 	except Exception as e:
+# 		pass
+# 		if i == 9:
+# 			print "No puedo encontrar la FPGA"
+# 			quit()
 
-print "FPGA encontrada en puerto COM" + com
+# print "FPGA encontrada en puerto COM" + com
 
-################################################################################
-# envio palabra inicio de programacion
-################################################################################
+# ret = ser.write(StartSignal)
 
 print "Abriendo el archivo " + file
 
@@ -78,8 +90,7 @@ except Exception as e:
 while True:
     line = f.readline()[:8] 	# trunco el \n, leyendo solo los 8 hexas del opcode
     if not line: break
-    print line
-    sent = ser.write(line)
+    sendInstruction(line)
 
 print "Envio del programa ensamblador finalizado"
 
@@ -89,72 +100,101 @@ Continuo: 1
 Paso a paso : 2
 """,
 
-modo = int(raw_input())
+while True:
 
-if modo == 1:
-	# modo continuo
+	try:
+		modo = int(raw_input())
+	except Exception as e:
+		print "ERROR, entrada incorrecta"
+		continue
 
-	print "Contador de Programa"
-	pc = read_32()
-	print_registro_32("PC: ", pc)	
+	if modo == 1:
+		# modo continuo
 
-	print "Ciclos de clock empleados"
-	ciclos = read_32()
-	print_registro_32("Ciclos: ", ciclos)
+		print "Contador de Programa"
+		pc = read_32()
+		print_registro_32("PC: ", pc)	
 
-	print "Registros del procesador"
-	for i in xrange(0,nro_registros):
+		print "Latches Intermedios IF/ID"
+		branch_1_2 = read32()
+		pc_out_1_2 = read32()
 
-		registro = read_32()
-		print_registro_32("Registro " + str(i) + ": ", registro)
+		print_registro_32("PC siguiente: ", pc_out_1_2)
+		print_registro_32("Branch target: ", branch_1_2)
+
+		print "Latches Intermedios ID/EX"
+
+		branch_2_3 = read32()
+		sign_extend_2_3 = read32()
+		bus1_2_3 = read32()
+		bus2_2_3 = read32()
+
+		print_registro_32("Sign extend: ", sign_extend_2_3)
+		print_registro_32("Branch target: ", branch_2_3)
+		
+		print_registro_32("Shamt: ", bus1_2_3[27:])
+		print_registro_32("Rt: ", bus1_2_3[22:27])
+		print_registro_32("Rs: ", bus1_2_3[17:22])
+		print_registro_32("Rd: ", bus1_2_3[12:17])
+
+		print "Buses de control"
+		print_registro_32("Execute bus: ", bus2_2_3[10:21])
+		print_registro_32("Memory bus: ", bus2_2_3[21:30])
+		print_registro_32("Writeback bus: ", bus2_2_3[30:])
+
+		print "Latches Intermedios EX/MEM"
+
+		reg2_3_4 = read32()
+		branch_3_4 = read32()
+		alu_out = read32()
+		bus1_3_4 = read32()
+
+		print_registro_32("ALU out: ", alu_out)
+		print_registro_32("Branch target: ", branch_3_4)
+		print_registro_32("Registro 2: ", reg2_3_4)
+
+		print_registro_32("Zero flag: ", bus1_3_4[31])
+		print_registro_32("Write register: ", bus1_3_4[26:31])
+		
+		print "Buses de control"
+		print_registro_32("Memory bus: ", bus1_3_4[15:24])
+		print_registro_32("Writeback bus: ", bus1_3_4[24:26])
+
+		print "Latches Intermedios MEM/WB"
+
+		read_data = read32()
+		alu_out_4_5 = read32()
+		bus1_4_5 = read32()
+
+		print_registro_32("Read data: ", read_data)
+		print_registro_32("ALU out: ", alu_out_4_5)
+
+		print_registro_32("Write register: ", bus1_4_5[25:30])
+
+		print "Buses de control"
+		print_registro_32("Writeback bus: ", bus1_4_5[30:])
+
+		print "Ciclos de clock empleados"
+		ciclos = read_32()
+		print_registro_32("Ciclos: ", ciclos)
+
+		print "Registros del procesador"
+		for i in xrange(0,nro_registros):
+
+			registro = read_32()
+			print_registro_32("Registro " + str(i) + ": ", registro)
 
 
-	print "Posiciones de memoria"
-	for i in xrange(0,posiciones_memoria):
+		print "Posiciones de memoria"
+		for i in xrange(0,posiciones_memoria):
 
-		memoria = read_32()
-		print_registro_32("Posicion " + str(i) + ": ", memoria)
+			memoria = read_32()
+			print_registro_32("Posicion " + str(i) + ": ", memoria)
 
-elif modo == 2:
-	# modo paso a paso
-	pass
-	# TO DO
-
-else:
-	# entrada incorrecta
-	print "ERROR, entrada incorrecta"
-
+	elif modo == 2:
+		# modo paso a paso
+		pass
+		# TO DO
 
 
 ser.close()
-
-
-
-# con teclado
-# while True:
-# 	raw_input("Presione una tecla para iniciar")
-
-# 	ser = serial.Serial('COM6')
-	
-# 	a = 0b01010101
-# 	sent = ser.write(chr(a))
-	
-# 	raw_input("Presione una tecla para iniciar")
-# 	a = 0b00000000
-# 	sent = ser.write(chr(a))
-
-# 	print "ACK"
-
-# 	x = ser.read()         
-# 	print "baja pc =", bin(ord(x))
-
-# 	x = ser.read()         
-# 	print "alta pc =", bin(ord(x))
-
-# 	x = ser.read()         
-# 	print "baja acc =", bin(ord(x))
-
-# 	x = ser.read()         
-# 	print "alta acc =", bin(ord(x))
-
-# 	ser.close()
